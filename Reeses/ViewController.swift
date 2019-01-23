861e084a40a4d21a11ecf231085983b2e5700b4f
user150978 <user150978@ny030.local>
2019-01-23 18:49:27 -0500

    changed text around and color

--- Reeses/ViewController.swift
+++ Reeses/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if messageLabel.text=="You got peanut butter in my chocolate!"{
+            messageLabel.text="You got chocolate in my peanut butter!"
+            messageLabel.textColor = UIColor.brown
+            messageLabel.textAlignment = NSTextAlignment.left
+        } else{
+            messageLabel.text="You got peanut butter in my chocolate!"
+            messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = NSTextAlignment.right
+        }
+    }
+    
 }
 
